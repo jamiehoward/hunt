7572f3f3e68d05e73240ae668a7eddd69ec84b1d
Jamie Howard <jamie@morrowmade.com>
2017-12-12 12:30:20 -0500

    added redirect for home to go to campaigns

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', function() {
+    return redirect('campaigns');
+});
 
 Route::group(['middleware' => 'auth'], function() {
     Route::resource('campaigns', 'CampaignController');
