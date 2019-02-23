ce75a82b6a35a632f1908cf5ba8c2534de1c236e
Jamie Howard <jamiehowardco@gmail.com>
2019-02-23 12:47:27 -0500

    Adding testing env to fix Dusk deployment error

---
+++ tests/Unit/ClueTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\Clue;
+use App\Models\Campaign;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class CampaignTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testClueCanBeOrdered()
+    {
+//
+    }
+}
